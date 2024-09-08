62542593523a4d9c7b2798d1e9ffcbb73921b120
Elkira01 <gilles.mbissileu@facsciences-uy1.cm>
2024-09-09 00:05:35 +0100

    start company

--- src/Kernel/Landing/UserInterface/Controller/HomeController.php
+++ src/Kernel/Landing/UserInterface/Controller/HomeController.php
@@ -5,31 +5,11 @@ declare(strict_types=1);
 namespace App\Kernel\Landing\UserInterface\Controller;
 
 use App\SharedKernel\UserInterface\AppController;
-use Sulu\Bundle\WebsiteBundle\Controller\WebsiteController;
-use Sulu\Component\Content\Compat\StructureInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AppController
 {
-//    public function indexAction(
-//        StructureInterface $structure,
-//        $preview = false,
-//        $partial = false
-//    ): Response
-//    {
-//        $locale = $this->getRequest()->getLocale();
-//
-//        return $this->renderStructure(
-//            $structure,
-//            [
-//                'locale' => $locale,
-//            ],
-//            $preview,
-//            $partial
-//        );
-//    }
-
     public function home(
         Request $request
     ): Response
